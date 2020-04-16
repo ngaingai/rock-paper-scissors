ce8681e788d757c173f7e209bdfd1e79bd8ff682
Alex N <alex.ngai@gmail.com>
2020-04-16 15:47:24 +0900

    5 Round game complete

--- game.js
+++ game.js
@@ -4,26 +4,36 @@ function computerPlay() {
 }
 
 function oneRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
     let result = "You win!"
     if (playerSelection == 'rock') {
         if (computerSelection == 'paper') {
-            result = "You Lose!";
+            result = "You lose!";
         } else if (computerSelection == 'rock') {
             result = "It's a tie!";
         }
     } else if (playerSelection == 'paper') {
         if (computerSelection == 'scissors') {
-            result = "You Lose!";
+            result = "You lose!";
         } else if (computerSelection == 'paper') {
             result = "It's a tie!";
         }
     } else if (playerSelection == 'scissors') {
         if (computerSelection == 'rock') {
-            result = "You Lose!";
+            result = "You lose!";
         } else if (computerSelection == 'scissors') {
             result = "It's a tie!";
         }
     }
     return result;
+}
+
+function game() {
+    for (i = 1; i < 6; i++) {
+        let playerSelection = prompt("Round " + i + ": What's your move?");
+        let computerSelection = computerPlay();
+        console.log("[Round " + i + "] You: " + playerSelection.toLowerCase() + " CPU: " + computerSelection);
+        console.log(oneRound(playerSelection, computerSelection));
+    }
+
 }
