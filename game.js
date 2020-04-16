bce831d24d92da50f10581699b4143cc2c87e211
Alex N <alex.ngai@gmail.com>
2020-04-16 15:02:31 +0900

    one round logic complete

--- game.js
+++ game.js
@@ -2,3 +2,28 @@ function computerPlay() {
     let compMove = ['rock', 'paper', 'scissors'];
     return compMove[Math.floor((Math.random()*compMove.length))];
 }
+
+function oneRound(playerSelection, computerSelection) {
+    playerSelection.toLowerCase();
+    let result = "You win!"
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            result = "You Lose!";
+        } else if (computerSelection == 'rock') {
+            result = "It's a tie!";
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'scissors') {
+            result = "You Lose!";
+        } else if (computerSelection == 'paper') {
+            result = "It's a tie!";
+        }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            result = "You Lose!";
+        } else if (computerSelection == 'scissors') {
+            result = "It's a tie!";
+        }
+    }
+    return result;
+}
