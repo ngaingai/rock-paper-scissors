84e87a413bbd595a221bc3d55751048bfebf63cf
Alex N <alex.ngai@gmail.com>
2020-04-24 16:17:32 +0900

    works but needs optimization

--- game.js
+++ game.js
@@ -25,15 +25,49 @@ function oneRound(playerSelection, computerSelection) {
             result = "It's a tie!";
         }
     }
+
     return result;
 }
 
-function game() {
-    for (i = 1; i < 6; i++) {
-        let playerSelection = prompt("Round " + i + ": What's your move?");
-        let computerSelection = computerPlay();
-        console.log("[Round " + i + "] You: " + playerSelection.toLowerCase() + " CPU: " + computerSelection);
-        console.log(oneRound(playerSelection, computerSelection));
-    }
+let pScore = 0;
+let cScore = 0;
+let tie = 0;
 
-}
+const container = document.querySelector('#container');
+const results = document.querySelector('#results');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let computerSelection = computerPlay();
+        let playerSelection = button.id;
+        let result = oneRound(playerSelection, computerSelection);
+
+        if (result == "You win!") {
+                    pScore++;
+                } else if (result == "You lose!") {
+                    cScore++;
+                } else {
+                    tie++;
+        }
+
+        let p = document.createElement('p');
+        p.textContent = "You: " + playerSelection + " CPU: " + computerSelection + " -> " + result;
+        results.appendChild(p);
+        let cs = document.createElement('p');
+        cs.textContent = "Current Score: " + pScore + " - " + cScore + " - " + tie;
+        results.appendChild(cs);
+
+        if (pScore == 5) {
+            let final = document.createElement('p');
+            final.textContent = "You're the winner!";
+            results.appendChild(final);
+        } else if (cScore == 5) {
+            let final = document.createElement('p');
+            final.textContent = "Computer wins!";
+            results.appendChild(final);
+        }
+
+    })
+})
+
